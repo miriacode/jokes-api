e1300fe51dd55e871d39ad7e0520698d5e5f064f
miriacode <miriamacunaenciso@gmail.com>
2022-05-09 00:23:31 -0500

    Completed exercise

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -4,8 +4,14 @@ const mongoose = require("mongoose");
 //-----------------------------------------------------------------------------------------------
 //Model Joke
 const SchemaJoke = new mongoose.Schema({
-    setup: String,
-    punchline: String,
+    setup: {
+        type: String,
+        minlength: [10, "Setup must be at least 10 characters long"]
+    },
+    punchline:{
+        type:String,
+        minlength: [3, "Punchline must be at least 3 characters long"]
+    }
 }, { timestamps: true, versionKey:false });
 
 //Creating Object Joke in document: Jokes through SchemaJoke
